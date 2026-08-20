9ab8cd75c772a52f16fd7aa005e3c53ec0115edd
NguyenThiLieudc21v7n520 <lieudc21v7n520@vlvh.ctu.edu.vn>
2026-08-20 21:48:43 +0700

    Cai dat them moi contact

--- public/add.php
+++ public/add.php
@@ -1,5 +1,30 @@
 <?php
+
 require_once __DIR__ . '/../src/bootstrap.php';
+
+use CT275\Labs\Contact;
+
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $contactData = [
+        'name' => $_POST['name'] ?? '',
+        'phone' => $_POST['phone'] ?? '',
+        'notes' => $_POST['notes'] ?? '',
+    ];
+
+    $contact = new Contact($PDO);
+
+    $errors = $contact->validate($contactData);
+
+    if (empty($errors)) {
+        $contact->fill($contactData);
+
+        if ($contact->save()) {
+            redirect('/');
+        }
+    }
+}
 
 include_once __DIR__ . '/../src/partials/header.php';
 ?>
@@ -23,7 +48,16 @@ include_once __DIR__ . '/../src/partials/header.php';
           <!-- Name -->
           <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" name="name" class="form-control<?= isset($errors['name']) ? ' is-invalid' : '' ?>" maxlen="255" id="name" placeholder="Enter Name" value="<?= isset($_POST['name']) ? html_escape($_POST['name']) : '' ?>" />
+
+            <input
+              type="text"
+              name="name"
+              class="form-control<?= isset($errors['name']) ? ' is-invalid' : '' ?>"
+              maxlen="255"
+              id="name"
+              placeholder="Enter Name"
+              value="<?= isset($_POST['name']) ? html_escape($_POST['name']) : '' ?>"
+            />
 
             <?php if (isset($errors['name'])) : ?>
               <span class="invalid-feedback">
@@ -32,10 +66,20 @@ include_once __DIR__ . '/../src/partials/header.php';
             <?php endif ?>
           </div>
 
+
           <!-- Phone -->
           <div class="mb-3">
             <label for="phone" class="form-label">Phone Number</label>
-            <input type="text" name="phone" class="form-control<?= isset($errors['phone']) ? ' is-invalid' : '' ?>" maxlen="255" id="phone" placeholder="Enter Phone" value="<?= isset($_POST['phone']) ? html_escape($_POST['phone']) : '' ?>" />
+
+            <input
+              type="text"
+              name="phone"
+              class="form-control<?= isset($errors['phone']) ? ' is-invalid' : '' ?>"
+              maxlen="255"
+              id="phone"
+              placeholder="Enter Phone"
+              value="<?= isset($_POST['phone']) ? html_escape($_POST['phone']) : '' ?>"
+            />
 
             <?php if (isset($errors['phone'])) : ?>
               <span class="invalid-feedback">
@@ -44,10 +88,17 @@ include_once __DIR__ . '/../src/partials/header.php';
             <?php endif ?>
           </div>
 
+
           <!-- Notes -->
           <div class="mb-3">
-            <label for="notes" class="form-label">Notes </label>
-            <textarea name="notes" id="notes" class="form-control<?= isset($errors['notes']) ? ' is-invalid' : '' ?>" placeholder="Enter notes (maximum character limit: 255)"><?= isset($_POST['notes']) ? html_escape($_POST['notes']) : '' ?></textarea>
+            <label for="notes" class="form-label">Notes</label>
+
+            <textarea
+              name="notes"
+              id="notes"
+              class="form-control<?= isset($errors['notes']) ? ' is-invalid' : '' ?>"
+              placeholder="Enter notes (maximum character limit: 255)"
+            ><?= isset($_POST['notes']) ? html_escape($_POST['notes']) : '' ?></textarea>
 
             <?php if (isset($errors['notes'])) : ?>
               <span class="invalid-feedback">
@@ -56,8 +107,16 @@ include_once __DIR__ . '/../src/partials/header.php';
             <?php endif ?>
           </div>
 
+
           <!-- Submit -->
-          <button type="submit" name="submit" class="btn btn-primary">Add Contact</button>
+          <button
+            type="submit"
+            name="submit"
+            class="btn btn-primary"
+          >
+            Add Contact
+          </button>
+
         </form>
 
       </div>
@@ -66,6 +125,7 @@ include_once __DIR__ . '/../src/partials/header.php';
   </div>
 
   <?php include_once __DIR__ . '/../src/partials/footer.php' ?>
+
 </body>
 
 </html>
